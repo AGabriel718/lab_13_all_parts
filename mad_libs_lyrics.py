52855541afc97ff3c92defdb456cbb3395b1ecdb
Student <student@ent-v321-004.local>
2013-05-08 19:54:53 -0400

    added files and edited lyrics and mad libs

---
+++ mad_libs_lyrics.py
@@ -0,0 +1,23 @@
+"""
+mad_libs_lyrics.py
+====
+* using your lyrics from lyrics.py, replace 4 words with variables
+* set your variables equal to strings at the top of the file
+* name the variables after the part of speech of the word they are replacing,
+  followed by a number:
+
+  adjective1 = 'tiny'
+  adjective2 = 'magical'
+  noun1 = 'ukulele'
+
+* print the new lyrics with variables to the console
+"""
+a = 'tropical'
+b = 'molten'
+c = 'hula'
+d = 'questions'
+
+print('On a '+ a +' island,')
+print('Underneath a '+ b +' lava moon.')
+print("Hangin' with the "+ c +" dancers,")
+print("Askin' "+ d +" cause' they got all the answers.")
